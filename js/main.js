f1867a2fe6df37090a928a700154cf5d2af39855
Mara Rúbia <153208889+mararubiaa@users.noreply.github.com>
2026-09-09 18:14:26 -0300

    Update video entries in libraryVideos array

--- js/main.js
+++ js/main.js
@@ -153,13 +153,13 @@
      type: "vimeo" | "youtube" | "behance"
   --------------------------------------------------------------------------- */
   var libraryVideos = [
-    { thumbnail: "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=600&q=80", type: "vimeo",   id: "1216433791", duration: "" },
-    { thumbnail: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=600&q=80", type: "vimeo",   id: "1216583316", duration: "" },
+    { thumbnail: "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=600&q=80", type: "vimeo", id: "1216433791", duration: "" },
+    { thumbnail: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=600&q=80", type: "vimeo", id: "1216583316", duration: "" },
     { thumbnail: "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=600&q=80", type: "vimeo", id: "1217156198", duration: "" },
     { thumbnail: "https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?w=600&q=80", type: "vimeo", id: "1217672305", duration: "" },
     { thumbnail: "https://images.unsplash.com/photo-1584017911766-d451b3d0e843?w=600&q=80", type: "vimeo", id: "1224102778", duration: "" },
-    { thumbnail: "https://images.unsplash.com/photo-1584017911766-d451b3d0e843?w=600&q=80", type: "vimeo", id: "1224946461", duration: "" },
-   { thumbnail: "https://images.unsplash.com/photo-1584017911766-d451b3d0e843?w=600&q=80",  type: "vimeo", id: "1224946639", duration: "" }
+    { thumbnail: "https://images.unsplash.com/photo-1628348068343-c6a848d2b6dd?w=600&q=80", type: "vimeo", id: "1224946461", duration: "" },
+   { thumbnail: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?w=600&q=80",  type: "vimeo", id: "1224946639", duration: "" }
   ];
 
   var libraryGrid = document.querySelector('[data-library-grid]');
